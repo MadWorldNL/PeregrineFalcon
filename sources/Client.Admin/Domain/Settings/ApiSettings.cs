c4f3e19a6df337979e37fe125f01327d9e4d5d6f
Oscar Veldman <oveldman@gmail.com>
2024-11-16 21:12:04 +0100

    Connect grpc with the admin project

---
+++ sources/Client.Admin/Domain/Settings/ApiSettings.cs
@@ -0,0 +1,8 @@
+namespace MadWorldNL.PeregrineFalcon.Domain.Settings;
+
+public class ApiSettings
+{
+    public const string Key = "Api";
+    
+    public string Endpoint { get; init; } = string.Empty;
+}
--- sources/Client.Admin/Program.cs
+++ sources/Client.Admin/Program.cs
@@ -1,11 +1,27 @@
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MadWorldNL.PeregrineFalcon;
+using MadWorldNL.PeregrineFalcon.Domain.Settings;
+using Microsoft.Extensions.Options;
+using Server.Controllers.Grpc;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.Configure<ApiSettings>(
+    builder.Configuration.GetSection(ApiSettings.Key));
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSingleton(services => 
+{ 
+    var httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
+    var baseUri = services.GetRequiredService<IOptions<ApiSettings>>().Value.Endpoint; 
+    var channel = GrpcChannel.ForAddress(baseUri, new GrpcChannelOptions { HttpClient = httpClient });
+    return new HealthService.HealthServiceClient(channel);
+});
 
 await builder.Build().RunAsync();
