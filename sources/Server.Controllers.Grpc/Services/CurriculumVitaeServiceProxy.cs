ef302b38cc230f604a56eabc00891ae3751d9a06
Oscar Veldman <oveldman@gmail.com>
2024-11-24 20:15:51 +0100

    Update contract with create and update CV

--- sources/Server.Controllers.Grpc/Services/CurriculumVitaeServiceProxy.cs
+++ sources/Server.Controllers.Grpc/Services/CurriculumVitaeServiceProxy.cs
@@ -5,11 +5,21 @@ namespace MadWorldNL.PeregrineFalcon.Services;
 
 public class CurriculumVitaeServiceProxy : CurriculumVitaeService.CurriculumVitaeServiceBase
 {
+    public override Task<CreateCurriculumVitaeResponse> CreateCurriculumVitae(CreateCurriculumVitaeRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new CreateCurriculumVitaeResponse());
+    }
+
     public override Task<GetCurriculumVitaeResponse> GetCurriculumVitae(GetCurriculumVitaeRequest request, ServerCallContext context)
     {
         return Task.FromResult(new GetCurriculumVitaeResponse()
         {
             CurriculumVitae = new CurriculumVitae()
         });
+    }
+
+    public override Task<PatchCurriculumVitaeResponse> PatchCurriculumVitae(PatchCurriculumVitaeRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new PatchCurriculumVitaeResponse());
     }
 }
